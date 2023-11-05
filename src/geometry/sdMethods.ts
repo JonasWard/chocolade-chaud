46385996c7b29d3f2fea42f4f0dd397a84fb2ff9
JonasWard <43600893+JonasWard@users.noreply.github.com>
2023-11-05 12:33:46 +0100

    added scaling

--- src/geometry/sdMethods.ts
+++ src/geometry/sdMethods.ts
@@ -1,21 +1,24 @@
 import { Vector3 } from '@babylonjs/core';
 
-export const sdGyroid = (v: Vector3) => Math.sin(v.x) * Math.cos(v.y) + Math.sin(v.y) * Math.cos(v.z) + Math.sin(v.z) * Math.cos(v.x);
-export const sdSchwarzP = (v: Vector3) => Math.cos(v.x) + Math.cos(v.y) + Math.cos(v.z);
-export const sdSchwarzD = (v: Vector3) => Math.cos(v.x) * Math.cos(v.y) * Math.cos(v.z) - Math.sin(v.x) * Math.sin(v.y) * Math.sin(v.z);
-export const sdNeovius = (v: Vector3) => 3 * sdSchwarzP(v) - 4 * Math.cos(v.x) * Math.cos(v.y) * Math.cos(v.z);
+export const sdGyroid = (v: Vector3, scale = 1) =>
+  Math.sin(v.x * scale) * Math.cos(v.y * scale) + Math.sin(v.y * scale) * Math.cos(v.z * scale) + Math.sin(v.z * scale) * Math.cos(v.x * scale);
+export const sdSchwarzP = (v: Vector3, scale = 1) => Math.cos(v.x * scale) + Math.cos(v.y * scale) + Math.cos(v.z * scale);
+export const sdSchwarzD = (v: Vector3, scale = 1) =>
+  Math.cos(v.x * scale) * Math.cos(v.y * scale) * Math.cos(v.z * scale) - Math.sin(v.x * scale) * Math.sin(v.y * scale) * Math.sin(v.z * scale);
+export const sdNeovius = (v: Vector3, scale = 1) =>
+  3 * (Math.cos(v.x * scale) + Math.cos(v.y * scale) + Math.cos(v.z * scale)) - 4 * Math.cos(v.x * scale) * Math.cos(v.y * scale) * Math.cos(v.z * scale);
 
-export const sdSphere = (v: Vector3) => v.length() - 1;
-export const sdBox = (v: Vector3) => Math.max(Math.abs(v.x), Math.abs(v.y), Math.abs(v.z)) - 1;
-export const sdTorus = (v: Vector3) => {
+export const sdSphere = (v: Vector3, scale = 1) => v.length() - 1;
+export const sdBox = (v: Vector3, scale = 1) => Math.max(Math.abs(v.x * scale), Math.abs(v.y * scale), Math.abs(v.z * scale)) - 1;
+export const sdTorus = (v: Vector3, scale = 1) => {
   const r1 = 1;
   const r2 = 0.25;
-  const q = new Vector3(new Vector3(v.x, v.z).length() - r1, v.y);
+  const q = new Vector3(new Vector3(v.x * scale, v.z * scale).length() - r1, v.y * scale);
   return q.length() - r2;
 };
-export const sdCylinder = (v: Vector3) => {
+export const sdCylinder = (v: Vector3, scale = 1) => {
   const r = 1;
-  const q = new Vector3(v.x, new Vector3(v.y, v.z).length());
+  const q = new Vector3(v.x * scale, new Vector3(v.y * scale, v.z * scale).length());
   return new Vector3(q.length() - r, q.y).length();
 };
 
@@ -40,7 +43,7 @@ export interface IDistanceData {
   scale: number;
 }
 
-const distanceMap = (dm: DistanceMethodType): ((v: Vector3) => number) => {
+const distanceMap = (dm: DistanceMethodType): ((v: Vector3, s: number) => number) => {
   switch (dm) {
     case DistanceMethodType.SDGyroid:
       return sdGyroid;
